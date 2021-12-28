263a9a4fdb07e01d8f79674bb05e10b6e20dc455
DidDog <94751052+Did-Dog@users.noreply.github.com>
2021-12-28 11:07:39 +0530

    Update VideoIdExtractor.java

--- VideoIdExtractor.java
+++ VideoIdExtractor.java
@@ -6,10 +6,10 @@ package diddog.youtube;
 import java.util.regex.*;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
-import Java.lang.*;
-import Java.lang.String;
+import java.lang.*;
+import java.lang.String;
 
-public static class VideoIDExtractor {
+public class VideoIDExtractor {
 
 	public static String getVideoId(String videoUrl) {
 		String videoId = "";
