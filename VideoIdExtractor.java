4f66c0ce855a1c15f6c7614fe6768f3d3793ca41
DidDog <94751052+Did-Dog@users.noreply.github.com>
2021-12-28 10:59:48 +0530

    Update VideoIdExtractor.java

--- VideoIdExtractor.java
+++ VideoIdExtractor.java
@@ -8,4 +8,15 @@ import Java.lang.String;
 
 public static class VideoIDExtractor {
 
+	public static String getVideoId(String videoUrl) {
+		String videoId = "";
+		String regex = "http(?:s)?:\\/\\/(?:m.)?(?:www\\.)?youtu(?:\\.be\\/|be\\.com\\/(?:watch\\?(?:feature=youtu.be\\&)?v=|v\\/|embed\\/|user\\/(?:[\\w#]+\\/)+))([^&#?\\n]+)";
+		Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+		Matcher matcher = pattern.matcher(videoUrl);
+		if(matcher.find()){
+			videoId = matcher.group(1);
+		}
+		return videoId;
+	}
+
 }
